eeb2488702a98644aa571c3ca6ec9f64b712f78d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "dedicated_hardware_security_modules" {
-  description = "All dedicated_hardware_security_module resources"
-  value       = azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules
-}
 output "dedicated_hardware_security_modules_location" {
-  description = "List of location values across all dedicated_hardware_security_modules"
-  value       = [for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : v.location]
+  description = "Map of location values across all dedicated_hardware_security_modules, keyed the same as var.dedicated_hardware_security_modules"
+  value       = { for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : k => v.location }
 }
 output "dedicated_hardware_security_modules_management_network_profile" {
-  description = "List of management_network_profile values across all dedicated_hardware_security_modules"
-  value       = [for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : v.management_network_profile]
+  description = "Map of management_network_profile values across all dedicated_hardware_security_modules, keyed the same as var.dedicated_hardware_security_modules"
+  value       = { for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : k => v.management_network_profile }
 }
 output "dedicated_hardware_security_modules_name" {
-  description = "List of name values across all dedicated_hardware_security_modules"
-  value       = [for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : v.name]
+  description = "Map of name values across all dedicated_hardware_security_modules, keyed the same as var.dedicated_hardware_security_modules"
+  value       = { for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : k => v.name }
 }
 output "dedicated_hardware_security_modules_network_profile" {
-  description = "List of network_profile values across all dedicated_hardware_security_modules"
-  value       = [for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : v.network_profile]
+  description = "Map of network_profile values across all dedicated_hardware_security_modules, keyed the same as var.dedicated_hardware_security_modules"
+  value       = { for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : k => v.network_profile }
 }
 output "dedicated_hardware_security_modules_resource_group_name" {
-  description = "List of resource_group_name values across all dedicated_hardware_security_modules"
-  value       = [for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : v.resource_group_name]
+  description = "Map of resource_group_name values across all dedicated_hardware_security_modules, keyed the same as var.dedicated_hardware_security_modules"
+  value       = { for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : k => v.resource_group_name }
 }
 output "dedicated_hardware_security_modules_sku_name" {
-  description = "List of sku_name values across all dedicated_hardware_security_modules"
-  value       = [for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : v.sku_name]
+  description = "Map of sku_name values across all dedicated_hardware_security_modules, keyed the same as var.dedicated_hardware_security_modules"
+  value       = { for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : k => v.sku_name }
 }
 output "dedicated_hardware_security_modules_stamp_id" {
-  description = "List of stamp_id values across all dedicated_hardware_security_modules"
-  value       = [for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : v.stamp_id]
+  description = "Map of stamp_id values across all dedicated_hardware_security_modules, keyed the same as var.dedicated_hardware_security_modules"
+  value       = { for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : k => v.stamp_id }
 }
 output "dedicated_hardware_security_modules_tags" {
-  description = "List of tags values across all dedicated_hardware_security_modules"
-  value       = [for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : v.tags]
+  description = "Map of tags values across all dedicated_hardware_security_modules, keyed the same as var.dedicated_hardware_security_modules"
+  value       = { for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : k => v.tags }
 }
 output "dedicated_hardware_security_modules_zones" {
-  description = "List of zones values across all dedicated_hardware_security_modules"
-  value       = [for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : v.zones]
+  description = "Map of zones values across all dedicated_hardware_security_modules, keyed the same as var.dedicated_hardware_security_modules"
+  value       = { for k, v in azurerm_dedicated_hardware_security_module.dedicated_hardware_security_modules : k => v.zones }
 }
 
